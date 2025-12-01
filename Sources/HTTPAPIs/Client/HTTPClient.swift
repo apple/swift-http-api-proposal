fd9652c2c997209e2be526b4b231ec89c5e4a431
Guoye Zhang <guoye_zhang@apple.com>
2025-12-01 15:59:49 -0800

    Remove old convenience

--- Sources/HTTPAPIs/Client/HTTPClient.swift
+++ Sources/HTTPAPIs/Client/HTTPClient.swift
@@ -64,43 +64,6 @@ public protocol HTTPClient<RequestConcludingWriter, ResponseConcludingReader>: ~
 }
 
 @available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *)
-extension HTTPClient where Self: ~Copyable {
-    /// Performs an HTTP request and processes the response.
-    ///
-    /// This method executes the HTTP request with the specified configuration and event
-    /// handler, then invokes the response handler when the response headers are received.
-    /// The request and response bodies are streamed using the client's writer and reader types.
-    ///
-    /// - Parameters:
-    ///   - request: The HTTP request headers to send.
-    ///   - body: The optional request body to send. When `nil`, no body is sent.
-    ///   - configuration: The configuration settings for this request.
-    ///   - eventHandler: The handler for processing events during request execution.
-    ///   - responseHandler: The closure to process the response. This closure is invoked
-    ///     when the response headers are received and can read the response body.
-    ///
-    /// - Returns: The value returned by the response handler closure.
-    ///
-    /// - Throws: An error if the request fails or if the response handler throws.
-    public func perform<Return>(
-        request: HTTPRequest,
-        body: consuming HTTPClientRequestBody<RequestConcludingWriter>? = nil,
-        configuration: HTTPClientConfiguration = .init(),
-        eventHandler: consuming some HTTPClientEventHandler & ~Escapable & ~Copyable =
-            DefaultHTTPClientEventHandler(),
-        responseHandler: (HTTPResponse, consuming ResponseConcludingReader) async throws -> Return,
-    ) async throws -> Return {
-        try await self.perform(
-            request: request,
-            body: body,
-            configuration: configuration,
-            eventHandler: eventHandler,
-            responseHandler: responseHandler
-        )
-    }
-}
-
-@available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *)
 extension HTTPClient {
     #if canImport(Darwin)
     /// Performs an HTTP request and processes the response.
