b0fb923dc6ddbef57cd1404f0ebc979bd75f8b3e
Honza Dvorsky <honza@apple.com>
2025-12-05 15:46:38 +0100

    PR feedback

--- Sources/HTTPAPIs/Client/HTTPClientRequestBody.swift
+++ Sources/HTTPAPIs/Client/HTTPClientRequestBody.swift
@@ -19,10 +19,7 @@ import AsyncStreaming
 /// ``HTTPClientRequestBody`` wraps a closure the encapsulates the logic
 /// to write a request body. It also contains extra hints and inputs to inform
 /// the custom request body writing.
-///
-/// Prefer passing restartable bodies to HTTP clients, as that allows more
-/// robust handling of retries and redirects.
-///
+/// 
 /// ## Usage
 ///
 /// ### Seekable bodies
@@ -50,55 +47,20 @@ import AsyncStreaming
 ///     // Start writing contents into writer from the beginning
 /// }
 /// ```
-///
-/// ### Non-restartable bodies
-///
-/// In the rare case when the source of the request body bytes cannot be restarted at all, and
-/// can only be written once, use a non-restartable body.
-///
-/// > Important: Non-restartable bodies limit the HTTP client's ability to handle redirects and retries.
-///
-/// ```swift
-/// let body: HTTPClientRequestBody = .nonrestartable { writer in
-///     // Start writing contents into writer from the beginning.
-///     // The closure will be called at most once.
-/// }
-/// ```
 @available(macOS 26.0, iOS 26.0, watchOS 26.0, tvOS 26.0, visionOS 26.0, *)
 public struct HTTPClientRequestBody<Writer>: Sendable, ~Copyable
 where Writer: ConcludingAsyncWriter & ~Copyable, Writer.Underlying.WriteElement == UInt8, Writer.FinalElement == HTTPFields?, Writer: SendableMetatype
 {
-    /// Extra information about the capabilities of the request body.
-    public struct Hints: Sendable {
+    /// The body can be asked to restart writing from an arbitrary offset.
+    public var isSeekable: Bool
 
-        /// The body can be asked to restart from the beginning.
-        public var isRestartable: Bool
-
-        /// The body can be asked to restart writing from an arbitrary offset.
-        public var isSeekable: Bool
-
-        /// The length of the body is known upfront and can be specified in
-        /// the `content-length` header field.
-        public var knownLength: Int64?
-
-        /// Creates new hints.
-        /// - Parameters:
-        ///   - isRestartable: The body can be asked to restart from the beginning.
-        ///   - isSeekable: The body can be asked to restart writing from an arbitrary offset.
-        ///   - knownLength: The length of the body is known upfront and can be specified in
-        ///     the `content-length` header field.
-        public init(
-            isRestartable: Bool,
-            isSeekable: Bool,
-            knownLength: Int64?
-        ) {
-            self.isRestartable = isRestartable
-            self.isSeekable = isSeekable
-            self.knownLength = knownLength
-        }
-    }
+    /// The length of the body is known upfront and can be specified in
+    /// the `content-length` header field.
+    public var knownLength: Int64?
 
     /// Inputs into the body creation closure.
+    /// 
+    /// This information is provided by the HTTP client implementation and passed with the writer.
     public struct Inputs: Sendable {
 
         /// The offset from which to start writing the body.
@@ -114,24 +76,22 @@ where Writer: ConcludingAsyncWriter & ~Copyable, Writer.Underlying.WriteElement 
         }
     }
 
-    /// Extra information about the capabilities of the request body.
-    public let hints: Hints
-
     /// The underlying logic for writing the request obdy.
     private let writeBody: @Sendable (Inputs, consuming Writer) async throws -> Void
 
-    /// The number of writeBody closure invocations.
-    private var writeCount: Int = 0
-
     /// Creates a new body.
     /// - Parameters:
-    ///   - hints: Extra information about the capabilities of the request body.
+    ///   - isSeekable: The body can be asked to restart writing from an arbitrary offset.
+    ///   - knownLength: The length of the body is known upfront and can be specified in
+    ///     the `content-length` header field.
     ///   - writeBody: The closure that writes the request body into the writer.
     internal init(
-        hints: Hints,
+        isSeekable: Bool,
+        knownLength: Int64?,
         writeBody: @escaping @Sendable (Inputs, consuming Writer) async throws -> Void
     ) {
-        self.hints = hints
+        self.isSeekable = isSeekable
+        self.knownLength = knownLength
         self.writeBody = writeBody
     }
 
@@ -141,10 +101,6 @@ where Writer: ConcludingAsyncWriter & ~Copyable, Writer.Underlying.WriteElement 
     ///   - writer: The destination into which to write the body.
     /// - Throws: An error thrown from the body closure.
     public mutating func write(inputs: Inputs, writer: consuming Writer) async throws {
-        if !hints.isRestartable && writeCount > 0 {
-            throw HTTPClientRequestBodyError.restartingNonrestartable
-        }
-        writeCount += 1
         try await writeBody(inputs, writer)
     }
 }
@@ -155,15 +111,10 @@ private enum HTTPClientRequestBodyError: Error, CustomStringConvertible {
     /// Client tried to seek a non-seekable body to a non-0 offset.
     case seekingNonSeekable
 
-    /// Client tried to restart a non-restartable body.
-    case restartingNonrestartable
-
     var description: String {
         switch self {
         case .seekingNonSeekable:
             "HTTPClientRequestBody: tried to seek to a non-0 offset in a non-seekable body"
-        case .restartingNonrestartable:
-            "HTTPClientRequestBody: tried to restart a non-restartable body"
         }
     }
 }
@@ -188,11 +139,8 @@ extension HTTPClientRequestBody {
         _ body: @escaping @Sendable (consuming Writer) async throws -> Void
     ) -> Self {
         Self.init(
-            hints: .init(
-                isRestartable: true,
-                isSeekable: false,
-                knownLength: nil
-            ),
+            isSeekable: false,
+            knownLength: knownLength,
             writeBody: { inputs, writer in
                 guard inputs.offset == 0 else {
                     throw HTTPClientRequestBodyError.seekingNonSeekable
@@ -219,43 +167,10 @@ extension HTTPClientRequestBody {
         _ body: @escaping @Sendable (Int64, consuming Writer) async throws -> Void
     ) -> Self {
         Self.init(
-            hints: .init(
-                isRestartable: true,
-                isSeekable: false,
-                knownLength: nil
-            ),
+            isSeekable: true,
+            knownLength: knownLength,
             writeBody: { inputs, writer in
                 try await body(inputs.offset, writer)
-            }
-        )
-    }
-
-    /// A nonrestartable request body that can be consumed up to once, from the beginning.
-    ///
-    /// > Important: This case is used when the body can only ever be produced once. Due to how
-    /// > that limits the HTTP client's ability to handle redirects and restarts, it is always
-    /// > encouraged to use restartable and seekable bodies.
-    ///
-    /// - Parameters:
-    ///   - knownLength: The length of the body is known upfront and can be specified in
-    ///     the `content-length` header field.
-    ///   - body: The closure that writes the request body using the provided writer.
-    ///     - writer: The closure that writes the request body using the provided writer.
-    public static func nonrestartable(
-        knownLength: Int64? = nil,
-        _ body: @escaping @Sendable (consuming Writer) async throws -> Void
-    ) -> Self {
-        Self.init(
-            hints: .init(
-                isRestartable: false,
-                isSeekable: false,
-                knownLength: nil
-            ),
-            writeBody: { inputs, writer in
-                guard inputs.offset == 0 else {
-                    throw HTTPClientRequestBodyError.seekingNonSeekable
-                }
-                try await body(writer)
             }
         )
     }
