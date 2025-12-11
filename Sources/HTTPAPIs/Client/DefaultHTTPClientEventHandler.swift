46d2b72db8a1096c1f1477567b4e272be84bb6e0
Guoye Zhang <guoye_zhang@apple.com>
2025-12-11 10:17:47 -0800

    Remove @frozen

--- Sources/HTTPAPIs/Client/DefaultHTTPClientEventHandler.swift
+++ Sources/HTTPAPIs/Client/DefaultHTTPClientEventHandler.swift
@@ -13,7 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 @available(macOS 26.2, iOS 26.2, watchOS 26.2, tvOS 26.2, visionOS 26.2, *)
-@usableFromInline @frozen
+@usableFromInline
 struct DefaultHTTPClientEventHandler: HTTPClientEventHandler, ~Copyable {
     @inlinable
     init() {}
