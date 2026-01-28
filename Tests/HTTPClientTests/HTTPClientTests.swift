d8a472f161f6d953839021c7df8be57af5e58792
Xyan Bhatnagar <x_bhatnagar@apple.com>
2026-01-28 14:12:56 -0800

    Add echo test
    Also use predefined header names instead of manual initialization.

--- Tests/HTTPClientTests/HTTPClientTests.swift
+++ Tests/HTTPClientTests/HTTPClientTests.swift
@@ -18,6 +18,7 @@ import HTTPServer
 import HTTPTypes
 import Testing
 import Logging
+import Synchronization
 
 let testsEnabled: Bool = {
     #if canImport(Darwin)
@@ -56,26 +57,6 @@ actor TestHTTPServer {
         running = true
         Task {
             try await server.serve { request, requestContext, requestBodyAndTrailers, responseSender in
-                // Collect the headers that were sent in with the request
-                var headers: [String: [String]] = [:]
-                for field in request.headerFields {
-                    if var header = headers[field.name.rawName] {
-                        header.append(field.value)
-                    } else {
-                        headers[field.name.rawName] = [field.value]
-                    }
-                }
-                
-                // Parse the body as a UTF8 string
-                let (body, _) = try await requestBodyAndTrailers.collect(upTo: 1024) { span in
-                    return String(copying: try UTF8Span(validating: span))
-                }
-                
-                // Construct a default response
-                let response = TestServerResponse(headers: headers, body: body)
-                let response_data = try JSONEncoder().encode(response)
-                let response_span = response_data.span
-                
                 switch request.path {
                 case "/get":
                     // Bad method
@@ -84,6 +65,26 @@ actor TestHTTPServer {
                         try await writer.writeAndConclude("Incorrect method".utf8.span, finalElement: nil)
                         return
                     }
+                    
+                    // Collect the headers that were sent in with the request
+                    var headers: [String: [String]] = [:]
+                    for field in request.headerFields {
+                        if var header = headers[field.name.rawName] {
+                            header.append(field.value)
+                        } else {
+                            headers[field.name.rawName] = [field.value]
+                        }
+                    }
+                    
+                    // Parse the body as a UTF8 string
+                    let (body, _) = try await requestBodyAndTrailers.collect(upTo: 1024) { span in
+                        return String(copying: try UTF8Span(validating: span))
+                    }
+                    
+                    // Construct a response
+                    let response = TestServerResponse(headers: headers, body: body)
+                    let response_data = try JSONEncoder().encode(response)
+                    let response_span = response_data.span
                     
                     // OK
                     let writer = try await responseSender.send(HTTPResponse(status: .ok))
@@ -96,12 +97,32 @@ actor TestHTTPServer {
                         return
                     }
                     
+                    // Collect the headers that were sent in with the request
+                    var headers: [String: [String]] = [:]
+                    for field in request.headerFields {
+                        if var header = headers[field.name.rawName] {
+                            header.append(field.value)
+                        } else {
+                            headers[field.name.rawName] = [field.value]
+                        }
+                    }
+                    
+                    // Parse the body as a UTF8 string
+                    let (body, _) = try await requestBodyAndTrailers.collect(upTo: 1024) { span in
+                        return String(copying: try UTF8Span(validating: span))
+                    }
+                    
+                    // Construct a response
+                    let response = TestServerResponse(headers: headers, body: body)
+                    let response_data = try JSONEncoder().encode(response)
+                    let response_span = response_data.span
+                    
                     // OK
                     let writer = try await responseSender.send(HTTPResponse(status: .ok))
                     try await writer.writeAndConclude(response_span, finalElement: nil)
                 case "/gzip":
                     // OK
-                    let response_headers = HTTPFields([HTTPField(name: .init("Content-Encoding")!, value: "gzip")])
+                    let response_headers = HTTPFields([HTTPField(name: .contentEncoding, value: "gzip")])
                     let writer = try await responseSender.send(HTTPResponse(status: .ok, headerFields: response_headers))
                     
                     // "TEST\n" as gzip-encode
@@ -110,7 +131,7 @@ actor TestHTTPServer {
                     try await writer.writeAndConclude(bytes.span, finalElement: nil)
                 case "/brotli":
                     // OK
-                    let response_headers = HTTPFields([HTTPField(name: .init("Content-Encoding")!, value: "br")])
+                    let response_headers = HTTPFields([HTTPField(name: .contentEncoding, value: "br")])
                     let writer = try await responseSender.send(HTTPResponse(status: .ok, headerFields: response_headers))
                     
                     // "TEST\n" as brotli-encode
@@ -119,11 +140,11 @@ actor TestHTTPServer {
                     try await writer.writeAndConclude(bytes.span, finalElement: nil)
                 case "/301":
                     // Redirect to /get
-                    let writer = try await responseSender.send(HTTPResponse(status: .movedPermanently, headerFields: HTTPFields([HTTPField(name: .init("Location")!, value: "/get")])))
+                    let writer = try await responseSender.send(HTTPResponse(status: .movedPermanently, headerFields: HTTPFields([HTTPField(name: .location, value: "/get")])))
                     try await writer.writeAndConclude("".utf8.span, finalElement: nil)
                 case "/308":
                     // Redirect to /get
-                    let writer = try await responseSender.send(HTTPResponse(status: .permanentRedirect, headerFields: HTTPFields([HTTPField(name: .init("Location")!, value: "/get")])))
+                    let writer = try await responseSender.send(HTTPResponse(status: .permanentRedirect, headerFields: HTTPFields([HTTPField(name: .location, value: "/get")])))
                     try await writer.writeAndConclude("".utf8.span, finalElement: nil)
                 case "/404":
                     let writer = try await responseSender.send(HTTPResponse(status: .notFound))
@@ -131,6 +152,31 @@ actor TestHTTPServer {
                 case "/999":
                     let writer = try await responseSender.send(HTTPResponse(status: .init(integerLiteral: 999)))
                     try await writer.writeAndConclude("".utf8.span, finalElement: nil)
+                case "/echo":
+                    // Bad method
+                    if request.method != .post {
+                        let writer = try await responseSender.send(HTTPResponse(status: .methodNotAllowed))
+                        try await writer.writeAndConclude("Incorrect method".utf8.span, finalElement: nil)
+                        return
+                    }
+                
+                    // Needed since we are lacking call-once closures
+                    var responseSender = Optional(responseSender)
+
+                    _ = try await requestBodyAndTrailers.consumeAndConclude { reader in
+                        // Needed since we are lacking call-once closures
+                        var reader = Optional(reader)
+                        
+                        // This header stops MIME type sniffing, which can cause delays in receiving
+                        // the chunked bytes.
+                        let headers = HTTPFields([HTTPField(name: .xContentTypeOptions, value: "nosniff")])
+                        let responseBodyAndTrailers = try await responseSender.take()!.send(.init(status: .ok, headerFields: headers))
+                        try await responseBodyAndTrailers.produceAndConclude { responseBody in
+                            var responseBody = responseBody
+                            try await responseBody.write(reader.take()!)
+                            return nil
+                        }
+                    }
                 case "/hang":
                     // Wait for an hour (effectively never giving an answer)
                     try! await Task.sleep(for: .seconds(60 * 60))
@@ -449,6 +495,60 @@ struct HTTPClientTests {
             }
             
             #expect(count == 100)
+        }
+    }
+    
+    @Test(.enabled(if: testsEnabled))
+    @available(macOS 26.2, iOS 26.2, watchOS 26.2, tvOS 26.2, visionOS 26.2, *)
+    func echo() async throws {
+        // This header stops MIME type sniffing, which can cause delays in receiving
+        // the chunked bytes.
+        let headers = HTTPFields([HTTPField(name: .xContentTypeOptions, value: "nosniff")])
+        let request = HTTPRequest(
+            method: .post,
+            scheme: "http",
+            authority: "127.0.0.1:12345",
+            path: "/echo",
+            headerFields: headers
+        )
+        
+        // Used to ping-pong between the client-side writer and reader
+        let writerWaiting: Mutex<CheckedContinuation<Void, Never>?> = .init(nil)
+        
+        try await httpClient.perform(
+            request: request,
+            body: .restartable { writer in
+                try await writer.produceAndConclude { q in
+                    var q = q
+                    
+                    for _ in 0..<1000 {
+                        // TODO: There's a bug that prevents a single byte from being
+                        // successfully written out as a chunk. So write 2 bytes for now.
+                        try await q.write("AB".utf8.span)
+                        
+                        // Only proceed once the client receives the echo.
+                        await withCheckedContinuation { continuation in
+                            writerWaiting.withLock{ $0 = continuation }
+                        }
+                    }
+                    return nil
+                }
+            }
+        ) { response, responseBodyAndTrailers in
+            #expect(response.status == .ok)
+            try await responseBodyAndTrailers.consumeAndConclude { reader in
+                var numberOfChunks = 0
+                try await reader.forEach { span in
+                    numberOfChunks += 1
+                    #expect(span.count == 2)
+                    #expect(span[0] == UInt8(ascii: "A"))
+                    #expect(span[1] == UInt8(ascii: "B"))
+                    
+                    // Unblock the writer
+                    writerWaiting.withLock { $0!.resume() }
+                }
+                #expect(numberOfChunks == 1000)
+            }
         }
     }
 }
