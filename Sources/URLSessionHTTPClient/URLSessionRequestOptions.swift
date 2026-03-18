795aef54d0f5ccacc660266459548f26a177e598
Nakul Bajaj <nakul_bajaj@apple.com>
2026-03-18 13:12:23 -0700

    remove URLSession-specific request options protocols

--- Sources/URLSessionHTTPClient/URLSessionRequestOptions.swift
+++ Sources/URLSessionHTTPClient/URLSessionRequestOptions.swift
@@ -17,13 +17,7 @@ public import NetworkTypes
 
 /// The options for the URLSession HTTP client implementation.
 @available(macOS 26.2, iOS 26.2, watchOS 26.2, tvOS 26.2, visionOS 26.2, *)
-public struct URLSessionRequestOptions:
-    HTTPClientCapability.RedirectionHandler,
-    HTTPClientCapability.TLSSecurityHandler,
-    HTTPClientCapability.TLSVersionSelection,
-    HTTPClientCapability.DeclarativePathSelection,
-    HTTPClientCapability.HTTPVersionDiscovery,
-    HTTPClientCapability.Timeouts
+public struct URLSessionRequestOptions: HTTPClientCapability.TLSVersionSelection
 {
     public var redirectionHandler: (any HTTPClientRedirectionHandler)? = nil
 
