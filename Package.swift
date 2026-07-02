78d73695bb620b83594cbb6e4e4237af0cbfa9a0
Guoye Zhang <guoye_zhang@apple.com>
2026-07-02 08:33:25 -0700

    Update AHC tag

--- Package.swift
+++ Package.swift
@@ -51,9 +51,9 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.44.0"),
         .package(url: "https://github.com/apple/swift-configuration.git", from: "1.2.0"),
         .package(
-            url: "https://github.com/guoye-zhang/async-http-client.git",
-            revision: "ea8eccfb2cef417914ad50feec4814a14e7bbab5",
-            traits: ["ExperimentalHTTPAPIsSupport"]
+            url: "https://github.com/swift-server/async-http-client.git",
+            exact: "1.35.0",
+            traits: ["UnstableHTTPAPIsSupport"]
         ),
     ],
     targets: [
